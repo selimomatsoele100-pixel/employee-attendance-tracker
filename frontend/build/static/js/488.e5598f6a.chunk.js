e7ec21c84092994d1ffca7c95199767889f3a7f5
selimomatsoele100-pixel <selimomatsoele100@gmail.com>
2025-10-30 14:54:27 +0200

    Add frontend build for deployment

---
+++ frontend/build/static/js/488.e5598f6a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkattendance_frontend=self.webpackChunkattendance_frontend||[]).push([[488],{488:(e,n,t)=>{new WeakMap;new Set}}]);
+//# sourceMappingURL=488.e5598f6a.chunk.js.map
